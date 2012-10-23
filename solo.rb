a50f8f3d3201f2acc7371bba6325c3dab401613e
Nick Ethier <ncethier@gmail.com>
2012-10-22 22:53:28 -0600

    fix cookbook path

--- solo.rb
+++ solo.rb
@@ -1,2 +1,2 @@
 file_cache_path "/var/chef-solo"
-cookbook_path ["/var/chef-solo/cookbooks","/opt/cookbooks/logstash-test/vendor/chef","/opt/cookbooks"]
+cookbook_path ["/var/chef-solo/cookbooks","/opt/cookbooks/logstash-test/cookbooks","/opt/cookbooks"]
