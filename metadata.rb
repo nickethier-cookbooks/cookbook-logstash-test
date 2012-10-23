067093c2f252845a1f0f380ed61384e475f01230
Nick Ethier <ncethier@gmail.com>
2012-10-22 18:09:05 -0600

    add chef objs

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             "logstash-test"
+maintainer       "Nick Ethier"
+maintainer_email "ncethier@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures logstash-test"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
---
+++ solo.rb
@@ -0,0 +1,2 @@
+file_cache_path "/var/chef-solo"
+cookbook_path ["/var/chef-solo/cookbooks","/opt/cookbooks/logstash-test/vendor/chef","/opt/cookbooks"]
