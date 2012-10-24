c8c66dd290484797c77fc6ad4a97a09d95768e21
Nick Ethier <ncethier@gmail.com>
2012-10-24 16:44:46 -0600

    remove jruby-1.6.8

--- recipes/rvm.rb
+++ recipes/rvm.rb
@@ -1,10 +1,9 @@
 node['rvm']['user_installs'] = [
   {
     'user' => 'vagrant',
-    'default_ruby' => 'jruby-1.6.8',
+    'default_ruby' => 'jruby-1.7.0',
     'rubies' => [
       "1.9.3",
-      "jruby-1.6.8",
       "jruby-1.7.0"
     ]
   }
