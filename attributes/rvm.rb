eab3a8fbf7013ec8089f7299f664cbc188d88438
Nick Ethier <ncethier@gmail.com>
2012-10-24 22:53:08 -0600

    move rvm attributes

---
+++ attributes/rvm.rb
@@ -0,0 +1,10 @@
+default['rvm']['user_installs'] = [
+  {
+    'user' => 'vagrant',
+    'default_ruby' => 'jruby-1.7.0',
+    'rubies' => [
+      "1.9.3",
+      "jruby-1.7.0"
+    ]
+  }
+]
--- recipes/rvm.rb
+++ recipes/rvm.rb
@@ -1,12 +1,1 @@
-node['rvm']['user_installs'] = [
-  {
-    'user' => 'vagrant',
-    'default_ruby' => 'jruby-1.7.0',
-    'rubies' => [
-      "1.9.3",
-      "jruby-1.7.0"
-    ]
-  }
-]
-
 require_recipe "rvm::user_install"
