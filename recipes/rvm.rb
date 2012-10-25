a4954c1bb324fd718689797aefd7de0483df4b90
Nick Ethier <ncethier@gmail.com>
2012-10-25 11:04:35 -0600

    move attributes into recipe

--- recipes/rvm.rb
+++ recipes/rvm.rb
@@ -1,1 +1,12 @@
+node['rvm']['user_installs'] = [
+  {
+    'user' => 'vagrant',
+    'default_ruby' => 'jruby-1.7.0',
+    'rubies' => [
+      "1.9.3",
+      "jruby-1.7.0"
+    ]
+  }
+]
+
 require_recipe "rvm::user"
